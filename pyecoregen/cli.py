96d1a652d6f173658a2b0ed79899b7ca2760fb97
Mike Pagel <mike@mpagel.de>
2017-05-26 08:07:28 +0200

    added loglevel control

--- pyecoregen/cli.py
+++ pyecoregen/cli.py
@@ -3,6 +3,10 @@ import argparse
 
 import sys
 
+import logging
+
+import collections
+
 import pyecore.resources
 from pyecoregen.ecore import EcoreGenerator
 
@@ -26,10 +30,30 @@ def generate_from_cli(args):
         help="Path to directory, where output files are generated.",
         required=True
     )
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        help="Increase logging verbosity.",
+        action='count'
+    )
 
     parsed_args = parser.parse_args(args)
+
+    configure_logging(parsed_args)
     model = load_model(parsed_args.ecore_model)
     EcoreGenerator().generate(model, parsed_args.out_folder)
+
+
+def configure_logging(parsed_args):
+    loglevel_map = collections.defaultdict(lambda: logging.WARNING)
+    loglevel_map.update({
+        1: logging.INFO,
+        2: logging.DEBUG
+    })
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
+        level=loglevel_map[parsed_args.verbose]
+    )
 
 
 def load_model(ecore_model_path):
